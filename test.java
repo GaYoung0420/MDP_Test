40027f7e5a81e317e7422407e3b48faa767169f0
김가영 <63998250+GaYoung0420@users.noreply.github.com>
2021-07-15 14:21:30 +0900

    test.java add

---
+++ test.java
@@ -0,0 +1,13 @@
+package com.example.demo.Controller;
+
+public class test {
+    
+
+
+    public static void main(String[] args) {
+        
+        String test ="  입니다.";
+        String gayoung = "가영";
+        System.out.println(test +" "+gayoung);
+    }
+}
